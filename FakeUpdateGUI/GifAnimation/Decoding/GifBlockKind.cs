f199983b9d8f7127ec35e9c80fa79af627ecf04a
superkid200 <minh306@outlook.com.vn>
2019-03-17 13:52:41 +0700

    Fixed Windows 7 code

---
+++ FakeUpdateGUI/GifAnimation/Decoding/GifBlockKind.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfAnimatedGif.Decoding
+{
+    internal enum GifBlockKind
+    {
+        Control,
+        GraphicRendering,
+        SpecialPurpose,
+        Other
+    }
+}
---
+++ FakeUpdateGUI/GifAnimation/Decoding/GifTrailer.cs
@@ -0,0 +1,24 @@
+﻿namespace WpfAnimatedGif.Decoding
+{
+    internal class GifTrailer : GifBlock
+    {
+        internal const int TrailerByte = 0x3B;
+
+
+        private GifTrailer()
+        {
+        }
+
+
+        internal override GifBlockKind Kind
+        {
+            get { return GifBlockKind.Other; }
+        }
+
+
+        internal static GifTrailer ReadTrailer()
+        {
+            return new GifTrailer();
+        }
+    }
+}
